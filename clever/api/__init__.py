35dc76c419b384326a672c8bde1373792a76ac50
andrew.locklair <andrew.locklair@frogslayer.com>
2019-08-12 16:31:21 -0500

    Re-run override.sh

--- clever/api/__init__.py
+++ clever/api/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.data_api import DataApi
-from swagger_client.api.events_api import EventsApi
+from clever.api.data_api import DataApi
+from clever.api.events_api import EventsApi
